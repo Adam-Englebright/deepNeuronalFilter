52f7a479dbc6d90f556c156563a30dc2921f5ac0
adam <adamenglebright@rocketmail.com>
2024-05-05 15:50:00 +0100

    Fix audio garbling when audio is delayed by filter.

--- directional_audio/directional_audio.cpp
+++ directional_audio/directional_audio.cpp
@@ -46,8 +46,7 @@ int main() {
 
   // Create our DNF with appropriate settings:
   DNF dnf(dnf_layers, dnf_taps, rate_actual, dnf_act_method, dnf_debug_output, dnf_threads);
-  boost::circular_buffer<double> delay(2, 0);
-
+  
   audio.start(); // Start the capture and playback devices
   while (true) {
     audio.capturePeriod(); // Capture a period
@@ -55,7 +54,7 @@ int main() {
     // Each frame has 2 samples (L+R), each 2 bytes in size (S16_LE).
     // Therefore, the for loop must increment 4 bytes at a time.
     // If indexing the buffer, cast to a int16_t*, then this means increments of 2.
-    for (int i = buffer_size-2; i >= 0; i -= 2) {
+    for (int i = 0; i < buffer_size/2; i += 2) {
       // Convert samples to doubles
       double left_sample = (double)( ( (int16_t*)buffer )[i] ) / 32768.0;
       double right_sample = (double)( ( (int16_t*)buffer )[i+1] ) / 32768.0;
@@ -63,21 +62,16 @@ int main() {
       // Filter pre-processing
       double primary = (left_sample + right_sample) / 2;
       double reference = (left_sample - right_sample) / 2;
-      //std::cout << "Primary at buffer index " << i << ": " << ((int16_t*)buffer)[i] << std::endl;
 
       // Filter
-      //double output = dnf.filter(primary, reference);
-      delay.push_back(primary);
+      double output = dnf.filter(primary, reference);
 
       // Convert filtered signal back to S16_LE and put back in buffer for playback
-      //int16_t formatted_output = output * 32768.0;
-      int16_t formatted_output = delay[0] * 32768.0;
+      int16_t formatted_output = output * 32768.0;
       ((int16_t*)buffer)[i] = formatted_output;
       ((int16_t*)buffer)[i+1] = formatted_output;
-      //std::cout << "Output at buffer index " << i << ": " << ((int16_t*)buffer)[i] << std::endl;
     }
     
     audio.playbackPeriod(); // Playback the captured period
-    //break;
   }
 }
